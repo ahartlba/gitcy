77c900b192dd343ae94cc042f05df8a34eea6da5
Andreas Hartl-Bachinger <andreas.hartl-bachinger@gmx.at>
2024-02-26 17:35:17 +0100

    add push flag
    by specifiying -p flag you can direclty push

--- git_utils/cli/ctd_.py
+++ git_utils/cli/ctd_.py
@@ -1,13 +1,17 @@
 from argparse import ArgumentParser
 from git_utils import commit_with_title_and_description
+from git_utils.core.push import git_push
 
 
 def main():
     parser = ArgumentParser()
     parser.add_argument("-t", "--title", required=True, type=str)
     parser.add_argument("-d", "--description", required=False, default=None, type=str)
+    parser.add_argument("-p", "--push", required=False, action="store_true")
     args = parser.parse_args()
     commit_with_title_and_description(args.title, args.description)
+    if args.push:
+        git_push()
 
 
 if __name__ == "__main__":
---
+++ git_utils/core/push.py
@@ -0,0 +1,5 @@
+import subprocess
+
+
+def git_push():
+    subprocess.run("git push")
