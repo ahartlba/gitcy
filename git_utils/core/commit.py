9493e723f7d104844ff3c997894406cb365b6208
Andreas Hartl-Bachinger <andreas.hartl-bachinger@gmx.at>
2024-02-26 17:31:15 +0100

    bugfix
    seems to be working

--- git_utils/core/commit.py
+++ git_utils/core/commit.py
@@ -7,4 +7,4 @@ def commit_with_title_and_description(title: str, description: Optional[str] = N
         commit_msg = "\n\n".join((title, description))
     else:
         commit_msg = title
-    subprocess.run(f"git commit -m {commit_msg}")
+    subprocess.run(f'git commit -m "{commit_msg}"')
