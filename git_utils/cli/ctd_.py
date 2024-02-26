0dde4252f4b5a0ec0daa1b534c4490db5effcaef
Andreas Hartl-Bachinger <andreas.hartl-bachinger@gmx.at>
2024-02-26 17:43:25 +0100

    add description

--- git_utils/cli/ctd_.py
+++ git_utils/cli/ctd_.py
@@ -5,9 +5,28 @@ from git_utils.core.push import git_push
 
 def main():
     parser = ArgumentParser()
-    parser.add_argument("-t", "--title", required=True, type=str)
-    parser.add_argument("-d", "--description", required=False, default=None, type=str)
-    parser.add_argument("-p", "--push", required=False, action="store_true")
+    parser.add_argument(
+        "-t",
+        "--title",
+        required=True,
+        type=str,
+        help="Commit title (main commit msg)",
+    )
+    parser.add_argument(
+        "-d",
+        "--description",
+        required=False,
+        default=None,
+        type=str,
+        help="Description ... Additional Information",
+    )
+    parser.add_argument(
+        "-p",
+        "--push",
+        required=False,
+        action="store_true",
+        help="Directly push commit",
+    )
     args = parser.parse_args()
     commit_with_title_and_description(args.title, args.description)
     if args.push:
